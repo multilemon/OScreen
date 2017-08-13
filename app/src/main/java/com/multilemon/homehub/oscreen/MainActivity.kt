d2a187a168c2b182534bc8bc49e579fc887b0d65
multilemon <multi.blue.lemon@gmail.com>
2017-08-14 00:34:14 +0700

    First commit

---
+++ app/src/main/java/com/multilemon/homehub/oscreen/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.multilemon.homehub.oscreen
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupWebView()
+    }
+
+    fun setupWebView() {
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                view.loadUrl(url)
+                return false
+            }
+        }
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://www.google.com/")
+    }
+}
