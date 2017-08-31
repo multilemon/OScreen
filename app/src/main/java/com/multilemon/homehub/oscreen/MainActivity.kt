930db426ecf0391e9ee95ac27981ec2a8947189d
multilemon <multi.blue.lemon@gmail.com>
2017-08-31 21:01:53 +0700

    Try ChromeCustomTabs

--- app/src/main/java/com/multilemon/homehub/oscreen/MainActivity.kt
+++ app/src/main/java/com/multilemon/homehub/oscreen/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.multilemon.homehub.oscreen
 
+import android.net.Uri
 import android.os.Bundle
+import android.support.customtabs.CustomTabsIntent
 import android.support.v7.app.AppCompatActivity
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,7 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupWebView()
+//        setupWebView()
+        setupChromeCustomTabs()
     }
 
     fun setupWebView() {
@@ -22,7 +26,19 @@ class MainActivity : AppCompatActivity() {
                 return false
             }
         }
+        webView.webChromeClient = WebChromeClient()
         webView.settings.javaScriptEnabled = true
-        webView.loadUrl("https://www.google.com/")
+        webView.loadUrl("https://www.google.com/intl/en/chrome/demos/speech.html")
+    }
+
+    fun setupChromeCustomTabs() {
+        val url = "https://www.google.com/intl/en/chrome/demos/speech.html"
+        val builder = CustomTabsIntent.Builder().apply {
+            enableUrlBarHiding()
+            setShowTitle(false)
+        }
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(url))
+        finish()
     }
 }
